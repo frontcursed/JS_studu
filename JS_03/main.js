b0bc42ca56f1849275315cce5f44914cc1de6bec
Alexander <94293225+frontcursed@users.noreply.github.com>
2022-11-30 02:28:13 +0300

    Add files via upload

---
+++ JS_03/main.js
@@ -0,0 +1,51 @@
+// изменение состояние => проверить наличие обработчика func
+//subscriber, listener, handler
+// формирует специальный объект (info about event)
+// func (e)
+
+const sm = document.getElementById('small')
+const md = document.getElementById('medium')
+const bg = document.getElementById('big')
+
+const handler1 = (e) => {
+    e.stopPropagation()
+    alert('yo!')
+}
+
+const handler2 = (e) => {
+    e.stopPropagation()
+    alert('hey!')
+}
+const handler3 = (e) => {
+    e.stopPropagation()
+    alert('wow!')
+}
+const handler4 = (e) => {
+    if (e.currentTarget.id === 'small') {
+        alert(e.currentTarget.id)
+    }
+}
+const handler5 = (e) => {
+    if(e.target.tagName === 'BUTTON'){
+        alert(e.target.id)
+    }
+}
+
+/*
+sm.onclick = handler1
+sm.onclick = handler2
+sm.onclick = () => {alert('yo!'); alert('yo!!')}*/
+
+/*sm.addEventListener('click', handler1)
+sm.addEventListener('click', handler2)
+sm.removeEventListener('click', handler1)*/
+sm.addEventListener('click', handler1)
+md.addEventListener('click', handler2)
+bg.addEventListener('click', handler3)
+
+const a = document.getElementById('a')
+
+a.addEventListener('click', (e) =>{
+    e.preventDefault()
+    alert('preventDefault')
+})
